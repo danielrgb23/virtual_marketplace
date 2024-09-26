ea3d74f3b7dd2bd56182fbac5ed5000325b4b854
Daniel Jasmelino <danielrgb2@gmail.com>
2024-09-26 14:43:00 -0300

    feat: update login_screen

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,30 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:virtual_marketplace/models/user.dart';
 import 'package:virtual_marketplace/helpers/validators.dart';
-import 'package:virtual_marketplace/service/auth_service.dart';
+import 'package:virtual_marketplace/models/user_manager.dart';
 import 'package:virtual_marketplace/helpers/show_snack_bar.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatelessWidget {
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
 
-class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    AuthService authService = AuthService();
-    final TextEditingController emailController = TextEditingController();
-    final TextEditingController passController = TextEditingController();
-
-    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-    final GlobalKey<ScaffoldState> scaffoldkey = GlobalKey<ScaffoldState>();
-
     return Scaffold(
-      key: scaffoldkey,
+      key: scaffoldKey,
       appBar: AppBar(
         title: const Text('Entrar'),
         centerTitle: true,
@@ -34,85 +28,93 @@ class _LoginScreenState extends State<LoginScreen> {
           margin: const EdgeInsets.symmetric(horizontal: 16),
           child: Form(
             key: formKey,
-            child: ListView(
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              children: <Widget>[
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(hintText: "E-mail"),
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  validator: (email) {
-                    if (!emailValid(email!)) {
-                      return 'E-mail inválido';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  controller: passController,
-                  decoration: const InputDecoration(hintText: "Senha"),
-                  autocorrect: false,
-                  obscureText: true,
-                  validator: (senha) {
-                    if (senha!.isEmpty || senha.length < 6) {
-                      return "Senha inválida";
-                    }
-                    return null;
-                  },
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    style: const ButtonStyle(
-                      padding: WidgetStatePropertyAll(EdgeInsets.zero),
+            child: Consumer<UserManager>(
+              builder: (_, UserManager userManager, Widget? child) {
+                return ListView(
+                  padding: const EdgeInsets.all(16),
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: emailController,
+                      enabled: !userManager.loading,
+                      decoration: const InputDecoration(hintText: 'E-mail'),
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      validator: (email) {
+                        if (!emailValid(email!)) {
+                          return 'E-mail inválido';
+                        }
+                        return null;
+                      },
                     ),
-                    onPressed: () {},
-                    child: const Text("Esqueci minha senha"),
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                SizedBox(
-                  height: 44,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (formKey.currentState!.validate()) {
-                        context
-                            .read<AuthService>()
-                            .signIn(
-                                onSucess: () {
-                                  //TODO: FECHAR TELA DE LOGIN
-                                },
-                                onError: (e) {},
-                                user: UserData(
-                                    email: emailController.text,
-                                    password: passController.text))
-                            .then((String? error) {
-                          showSnackBar(context: context, mensagem: error!);
-                        });
-                      }
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateColor.resolveWith(
-                        (states) => const Color.fromARGB(255, 4, 125, 141),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      controller: passController,
+                      enabled: !userManager.loading,
+                      decoration: const InputDecoration(hintText: 'Senha'),
+                      autocorrect: false,
+                      obscureText: true,
+                      validator: (senha) {
+                        if (senha!.isEmpty || senha.length < 6) {
+                          return "Senha inválida";
+                        }
+                        return null;
+                      },
+                    ),
+                    child!,
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    SizedBox(
+                      height: 44,
+                      child: ElevatedButton(
+                        onPressed: userManager.loading
+                            ? null
+                            : () {
+                                if (formKey.currentState!.validate()) {
+                                  userManager.signIn(
+                                      user: UserData(
+                                          email: emailController.text,
+                                          password: passController.text),
+                                      onError: (error) {
+                                        showSnackBar(
+                                            context: context, mensagem: error!);
+                                      },
+                                      onSuccess: () {
+                                        // TODO: FECHAR TELA DE LOGIN
+                                      });
+                                }
+                              },
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateColor.resolveWith(
+                            (states) => const Color.fromARGB(255, 4, 125, 141),
+                          ),
+                        ),
+                        child: const Text(
+                          "Entrar",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500),
+                        ),
                       ),
                     ),
-                    child: const Text(
-                      "Entrar",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500),
+                  ],
+                );
+              },
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {
+                      // Sua lógica de esqueci minha senha aqui
+                    },
+                    child: const Text('Esqueci minha senha'),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
                     ),
-                  ),
-                )
-              ],
+                  )),
             ),
           ),
         ),
